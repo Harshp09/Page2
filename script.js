9bbd05ca53ccc3cfdca79ab8b78af5654319ea63
Harshp09 <113929405+Harshp09@users.noreply.github.com>
2023-07-01 16:42:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+function animateOnScroll() {
+    const elements = document.querySelectorAll('.fadeInAnimation');
+    elements.forEach((element) => {
+      if (isElementInViewport(element)) {
+        element.classList.add('animate');
+      }
+    });
+  }
+  function isElementInViewport(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  window.addEventListener('scroll', animateOnScroll);
+
+  window.addEventListener('DOMContentLoaded', animateOnScroll);
+  
